aadf8b90c599665ad1e9d9d00665c1286e938bc6
saras <sarasobekova@azet.sk>
2026-03-09 14:04:12 +0100

    pridanie php súborov

---
+++ otazky.php
@@ -0,0 +1,15 @@
+<?php
+
+$otazky = array (
+"Aký je dnes deň ?",
+"Aké je dnes počasie ?",
+"Čo budeš dnes robiť ?",
+);
+
+$odpovede = array (
+"Pondelok",
+"Slnečno",
+"To ešte netuším",
+);
+
+?>
